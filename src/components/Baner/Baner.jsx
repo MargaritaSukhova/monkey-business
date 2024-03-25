2ab0f46c61b39c7ab88acf2f3890521e32ed862a
MargaritaSukhova <margarytasu@gmail.com>
2024-03-25 02:26:57 +0200

    add baner

---
+++ src/components/Baner/Baner.jsx
@@ -0,0 +1,24 @@
+import Cross from "../../assets/icons/cross.svg"
+
+const Baner = () => {
+	return (
+		<div>
+			<ul>
+				<li>
+					<p>Destroy stereotypes</p>
+					<img src={Cross} alt="Cross icon" />
+				</li>
+				<li>
+					<p>HAVE NO LIMITS</p>
+					<img src={Cross} alt="Cross icon" />
+				</li>
+				<li>
+					<p>Break rules</p>
+					<img src={Cross} alt="Cross icon" />
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Baner;
